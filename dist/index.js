a7b0aea82a985ba0fe1034684978795dc440bbcd
nowyDEV <dominik.nowik@nordcloud.com>
2021-09-15 12:11:40 +0200

    cleanup

--- dist/index.js
+++ dist/index.js
@@ -9148,19 +9148,7 @@ const checkOutstandingTasks = __nccwpck_require__(3807);
 
 function run() {
   try {
-    // This should be a token with access to your repository scoped in as a secret.
-    // The YML workflow will need to set myToken with the GitHub Secret Token
-    // myToken: ${{ secrets.GITHUB_TOKEN }}
-    // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const myToken = core.getInput("myToken");
-
     const payload = github.context.payload;
-    payload.pull_request;
-
-    const octokit = github.getOctokit(myToken);
-
-    // You can also pass in additional options as a second parameter to getOctokit
-    // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
 
     const startTime = new Date().toISOString();
 
